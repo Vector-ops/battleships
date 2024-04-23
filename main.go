666d578effefdf28a9b47b6835f04615e816beb3
Vector-ops <sumith2827@gmail.com>
2024-04-23 19:12:28 +0530

    use bufio to read user input

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
 )
 
 var clear map[string]func()
@@ -37,16 +39,19 @@ func init() {
 
 func main() {
 	clearConsole()
-	// drawMap()
 	gameMap = make(map[Coordinates]string, 25)
 	fillMap(&gameMap)
 	var tries int = 5
-	var hit bool = false
+	var hit bool
 	for tries > 0 {
-		hit = userInput(&gameMap, hit, tries)
-		if !hit {
+		drawEmptyMap(hit, tries)
+		hit, e := userInput(&gameMap)
+		if !hit && !e {
 			tries--
 		}
+	}
+	if tries == 0 || tries < 0 {
+		drawMap(hit, tries, gameMap)
 	}
 }
 
@@ -58,29 +63,29 @@ func clearConsole() {
 }
 
 func drawEmptyMap(hit bool, tries int) {
+	clearConsole()
 	fmt.Printf("tries left: %d   hit: %t\n", tries, hit)
-	fmt.Println(" A  B  C  D  E")
-	fmt.Println("----------------")
-	fmt.Println("|  |  |  |  |  | 1")
-	fmt.Println("----------------")
-	fmt.Println("|  |  |  |  |  | 2")
-	fmt.Println("----------------")
-	fmt.Println("|  |  |  |  |  | 3")
-	fmt.Println("----------------")
-	fmt.Println("|  |  |  |  |  | 4")
-	fmt.Println("----------------")
-	fmt.Println("|  |  |  |  |  | 5")
+	fmt.Println("  A   B   C   D   E")
+	x := []string{"A", "B", "C", "D", "E"}
+	for i := range 5 {
+		fmt.Println("---------------------")
+		for _, j := range x {
+			if gameMap[Coordinates{x: j, y: i}] != "b" {
+				fmt.Printf("| %s ", gameMap[Coordinates{x: j, y: i}])
+			} else {
+				fmt.Printf("|   ")
+			}
+		}
+		fmt.Printf("| %d\n", i+1)
+	}
 	fmt.Println("---------------------")
 }
 
-func drawMap(hit bool, tries int) {
+func drawMap(hit bool, tries int, gameMap map[Coordinates]string) {
+	clearConsole()
 	fmt.Printf("tries left: %d   hit: %t\n", tries, hit)
 	x := []string{"A", "B", "C", "D", "E"}
 	fmt.Println("  A   B   C   D   E")
-
-	gameMap = make(map[Coordinates]string, 25)
-
-	fillMap(&gameMap)
 
 	for i := range 5 {
 		fmt.Println("---------------------")
@@ -105,18 +110,31 @@ func fillMap(gameMap *map[Coordinates]string) {
 	}
 }
 
-func userInput(gameMap *map[Coordinates]string, prevHit bool, tries int) bool {
-	// clearConsole()
-	drawMap(prevHit, tries)
+func userInput(gameMap *map[Coordinates]string) (bool, bool) {
+
 	var hit bool = false
 	var x string
 	var y int
 
+	r := bufio.NewReader(os.Stdin)
+
 	fmt.Println("Enter the coordinates in this format (A 1): ")
-	fmt.Scanf("%s %d", x, y)
+	inp, _, err := r.ReadLine()
+	if err != nil {
+		fmt.Printf("Invalid coordinates.\n")
+		return false, true
+	}
+	x = string(inp[0:1])
+	y, err = strconv.Atoi(string(inp[2:]))
+	if err != nil {
+		fmt.Printf("Invalid x coordinate")
+		return false, true
+	}
+
 	fmt.Println(x, y, (*gameMap)[Coordinates{x: x, y: y}])
-	if (*gameMap)[Coordinates{x: x, y: y}] == "b" {
+	if (*gameMap)[Coordinates{x: x, y: (y - 1)}] == "b" {
+		(*gameMap)[Coordinates{x: x, y: (y - 1)}] = "*"
 		hit = true
 	}
-	return hit
+	return hit, false
 }
