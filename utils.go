129799e5dd4dadb40363b3bc0b8c7f8b72b515ec
Vector-ops <sumith2827@gmail.com>
2024-05-12 10:35:09 +0530

    add ship placement utility functions

---
+++ utils.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math/rand"
+
+	"github.com/Vector-ops/battleships/enums"
+	"github.com/Vector-ops/battleships/types"
+)
+
+func getValidCoordinates(s types.ShipSize, gm types.GameMap) []types.ValidCoordinates {
+	size := 0
+	ship := []string{"s", "m", "l"}
+	var valid []types.ValidCoordinates
+	for i := range 5 {
+		var coord types.Coordinates
+		for j := range 5 {
+			if gm[types.Coordinates{X: j, Y: i}] == " " {
+				if size == 0 {
+					coord = types.Coordinates{X: j, Y: i}
+				}
+				size++
+				if size == int(s) {
+					valid = append(valid, types.ValidCoordinates{Direction: enums.Horizontal, StCd: coord, EdCd: types.Coordinates{X: j, Y: i}, Ship: ship[int(s)-1]})
+					size = 0
+				}
+			} else {
+				size = 0
+			}
+		}
+		size = 0
+	}
+	return valid
+}
+
+func PlaceShips(shipSize types.ShipSize, count int, gm *types.GameMap) {
+	validCoords := getValidCoordinates(shipSize, *gm)
+	for range count {
+		// pick a random coordinate
+		ind := rand.Intn(len(validCoords))
+		choice := validCoords[ind]
+
+		// remove chosen coordinate from the list of available coordinates
+		validCoords = append(validCoords, validCoords[:ind]...)
+		validCoords = append(validCoords, validCoords[:ind+1]...)
+		for i := choice.StCd.X; i <= choice.EdCd.X; i++ {
+			(*gm)[types.Coordinates{X: i, Y: choice.StCd.Y}] = choice.Ship
+		}
+	}
+}
