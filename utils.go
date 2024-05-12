6518eff6250285d24a15cb8e7677ab4d12065488
Vector-ops <sumith2827@gmail.com>
2024-05-12 10:53:08 +0530

    add vertical ship placements

--- utils.go
+++ utils.go
@@ -29,6 +29,24 @@ func getValidCoordinates(s types.ShipSize, gm types.GameMap) []types.ValidCoordi
 		}
 		size = 0
 	}
+	for i := range 5 {
+		var coord types.Coordinates
+		for j := range 5 {
+			if gm[types.Coordinates{X: i, Y: j}] == " " {
+				if size == 0 {
+					coord = types.Coordinates{X: i, Y: j}
+				}
+				size++
+				if size == int(s) {
+					valid = append(valid, types.ValidCoordinates{Direction: enums.Vertical, StCd: coord, EdCd: types.Coordinates{X: i, Y: j}, Ship: ship[int(s)-1]})
+					size = 0
+				}
+			} else {
+				size = 0
+			}
+		}
+		size = 0
+	}
 	return valid
 }
 
@@ -42,8 +60,15 @@ func PlaceShips(shipSize types.ShipSize, count int, gm *types.GameMap) {
 		// remove chosen coordinate from the list of available coordinates
 		validCoords = append(validCoords, validCoords[:ind]...)
 		validCoords = append(validCoords, validCoords[:ind+1]...)
-		for i := choice.StCd.X; i <= choice.EdCd.X; i++ {
-			(*gm)[types.Coordinates{X: i, Y: choice.StCd.Y}] = choice.Ship
+
+		if choice.Direction == enums.Horizontal {
+			for i := choice.StCd.X; i <= choice.EdCd.X; i++ {
+				(*gm)[types.Coordinates{X: i, Y: choice.StCd.Y}] = choice.Ship
+			}
+		} else {
+			for i := choice.StCd.Y; i <= choice.EdCd.Y; i++ {
+				(*gm)[types.Coordinates{X: choice.StCd.X, Y: i}] = choice.Ship
+			}
 		}
 	}
 }
